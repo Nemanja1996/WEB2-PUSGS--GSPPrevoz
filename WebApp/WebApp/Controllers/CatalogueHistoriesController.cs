04dbe3fcc3fba364581bdf04f91e26a11a3cf0fe
Nemanja Vasiljevic <nemanjav1996@hotmail.com>
2019-05-29 15:53:45 +0200

    Added two controllers

---
+++ WebApp/WebApp/Controllers/CatalogueHistoriesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    public class CatalogueHistoriesController : ApiController
+    {
+        //private ApplicationDbContext db = new ApplicationDbContext();
+        private IUnitOfWork db;
+
+        public CatalogueHistoriesController(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+        // GET: api/CatalogueHistories
+        public IEnumerable<CatalogueHistory> GetCatalogueHistory()
+        {
+            return db.CatalogueHistory.GetAll();
+        }
+
+        // GET: api/CatalogueHistories/5
+        [ResponseType(typeof(CatalogueHistory))]
+        public IHttpActionResult GetCatalogueHistory(int id)
+        {
+            CatalogueHistory catalogueHistory = db.CatalogueHistory.Get(id);
+            if (catalogueHistory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(catalogueHistory);
+        }
+
+        // PUT: api/CatalogueHistories/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCatalogueHistory(int id, CatalogueHistory catalogueHistory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != catalogueHistory.Id)
+            {
+                return BadRequest();
+            }
+
+            db.CatalogueHistory.Update(catalogueHistory);
+
+            try
+            {
+                db.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatalogueHistoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CatalogueHistories
+        [ResponseType(typeof(CatalogueHistory))]
+        public IHttpActionResult PostCatalogueHistory(CatalogueHistory catalogueHistory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CatalogueHistory.Add(catalogueHistory);
+            db.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = catalogueHistory.Id }, catalogueHistory);
+        }
+
+        // DELETE: api/CatalogueHistories/5
+        [ResponseType(typeof(CatalogueHistory))]
+        public IHttpActionResult DeleteCatalogueHistory(int id)
+        {
+            CatalogueHistory catalogueHistory = db.CatalogueHistory.Get(id);
+            if (catalogueHistory == null)
+            {
+                return NotFound();
+            }
+
+            db.CatalogueHistory.Remove(catalogueHistory);
+            db.Complete();
+
+            return Ok(catalogueHistory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CatalogueHistoryExists(int id)
+        {
+            return db.CatalogueHistory.GetAll().Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApp/WebApp/Controllers/CataloguesController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    public class CataloguesController : ApiController
+    {
+        //private ApplicationDbContext db = new ApplicationDbContext();
+        private IUnitOfWork db;
+
+        public CataloguesController(IUnitOfWork db)
+        {
+            this.db = db;
+        }
+        // GET: api/Catalogues
+        public IEnumerable<Catalogue> GetCatalogue()
+        {
+            return db.Catalogues.GetAll();
+        }
+
+        // GET: api/Catalogues/5
+        [ResponseType(typeof(Catalogue))]
+        public IHttpActionResult GetCatalogue(int id)
+        {
+            Catalogue catalogue = db.Catalogues.Get(id);
+            if (catalogue == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(catalogue);
+        }
+
+        // PUT: api/Catalogues/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCatalogue(int id, Catalogue catalogue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != catalogue.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Catalogues.Update(catalogue);
+
+            try
+            {
+                db.Complete();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatalogueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Catalogues
+        [ResponseType(typeof(Catalogue))]
+        public IHttpActionResult PostCatalogue(Catalogue catalogue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Catalogues.Add(catalogue);
+            db.Complete();
+
+            return CreatedAtRoute("DefaultApi", new { id = catalogue.Id }, catalogue);
+        }
+
+        // DELETE: api/Catalogues/5
+        [ResponseType(typeof(Catalogue))]
+        public IHttpActionResult DeleteCatalogue(int id)
+        {
+            Catalogue catalogue = db.Catalogues.Get(id);
+            if (catalogue == null)
+            {
+                return NotFound();
+            }
+
+            db.Catalogues.Remove(catalogue);
+            db.Complete();
+
+            return Ok(catalogue);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CatalogueExists(int id)
+        {
+            return db.Catalogues.GetAll().Count(e => e.Id == id) > 0;
+        }
+    }
+}
