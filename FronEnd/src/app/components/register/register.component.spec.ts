b86ab4c01529ddd4c27c6748fcb277ee8f800a2a
Nemanja Vasiljevic <nemanjav1996@hotmail.com>
2019-05-31 17:25:16 +0200

    Repeat commit

--- FronEnd/src/app/components/register/register.component.spec.ts
+++ FronEnd/src/app/components/register/register.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UnauthorizedUserComponent } from './unauthorized-user.component';
+import { RegisterComponent } from './register.component';
 
-describe('UnauthorizedUserComponent', () => {
-  let component: UnauthorizedUserComponent;
-  let fixture: ComponentFixture<UnauthorizedUserComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UnauthorizedUserComponent ]
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UnauthorizedUserComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ FronEnd/src/app/components/unauthorize-user/unauthorize-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorize-user',
+  templateUrl: './unauthorize-user.component.html',
+  styleUrls: ['./unauthorize-user.component.css']
+})
+export class UnauthorizeUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
