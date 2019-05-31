b914386ec68c3de1ab1f1aa84909b99974633ae5
Nemanja Vasiljevic <nemanjav1996@hotmail.com>
2019-05-31 10:35:59 +0200

    Gitignore commit

--- FronEnd/src/app/app.module.ts
+++ FronEnd/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router'
+
 import { AppComponent } from './app.component';
+import { UnauthorizedUserComponent } from './unauthorized-user/unauthorized-user.component';
+import { UnauthorizedHeaderComponent } from './unauthorized-header/unauthorized-header.component';
+import { UnauthorizedNavBarComponent } from './unauthorized-nav-bar/unauthorized-nav-bar.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './Interceptors/token.interceptor';
+
+const routes: Routes = [
+  {path: "", component: UnauthorizedUserComponent},
+  {path: "unauthorizedUser", component: UnauthorizedUserComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnauthorizedUserComponent,
+    UnauthorizedHeaderComponent,
+    UnauthorizedNavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
